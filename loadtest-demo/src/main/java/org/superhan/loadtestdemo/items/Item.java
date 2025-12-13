949a16f79bebffb513b93ef15ac8d0b5dfd5147e
superhan-dev <superhan-dev@gmail.com>
2025-12-13 19:37:14 +0900

    feat: 버그 수정및 설정 파일 수정

--- loadtest-demo/src/main/java/org/superhan/loadtestdemo/items/Item.java
+++ loadtest-demo/src/main/java/org/superhan/loadtestdemo/items/Item.java
@@ -2,13 +2,19 @@ package org.superhan.loadtestdemo.items;
 
 import jakarta.persistence.*;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Entity
 @Table(name = "items",
   indexes = {
     @Index(name = "idx_items_title", columnList = "title")
   }
 )
-public class Item {
+public class Item implements Serializable {
+  @Serial
+  private static final long serialVersionUID = 1L;
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
